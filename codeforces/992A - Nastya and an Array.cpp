3d62512fbe75da750b120a0ef2b0ee801c56950d
Carlos Mendoza <cmendoza.github@gmail.com>
2018-06-18 23:18:44 +0200

    Codeforces #488 (Div2) - Problem A

---
+++ codeforces/992A - Nastya and an Array.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Problem  : 992A - Nastya and an Array
+// Category : Set
+//============================================================================
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,x;
+    set<int> st;
+    while(cin >> n)
+    {
+        st.clear();
+        for(int i = 0; i < n; i++)
+        {
+            cin >> x;
+            if(x != 0)
+                st.insert(x);
+        }
+        cout << st.size() << '\n';
+    }
+    return 0;
+}
