93dbd6bd63d1f927608532487bf190fa63069d2a
Carlos Mendoza <cmendoza.github@gmail.com>
2018-06-18 02:41:41 +0200

    Codeforces #488 (Div2) - Problem B

--- codeforces/994B - Knights of a Polygonal Table.cpp
+++ codeforces/994B - Knights of a Polygonal Table.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Problem  : 994B - Knights of a Polygonal Table
-// Category : Greedy + Sortings
+// Category : Greedy + Sortings 
 //============================================================================
 #include <bits/stdc++.h>
 using namespace std;
