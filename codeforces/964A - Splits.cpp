1453c92d410d090cfadd5d155acf15a0ef2c1edd
Carlos Mendoza <cmendoza.github@gmail.com>
2018-07-10 22:24:17 +0200

    Codeforces #475 (Div2) - Problem A

---
+++ codeforces/964A - Splits.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Problem  : 964A - Splits
+// Category : Simple Math
+//============================================================================
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    int n;
+    while(cin >> n)
+        cout << n/2 + 1 << '\n';
+    return 0;
+}
