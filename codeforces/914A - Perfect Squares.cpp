d197ba6dd765d286fec225aca6a31bddbf9172e8
Carlos Mendoza <cmendoza.github@gmail.com>
2018-07-30 23:33:53 +0200

    Codeforces #499 (Div1+Div2) - Problem A

---
+++ codeforces/914A - Perfect Squares.cpp
@@ -0,0 +1,27 @@
+//============================================================================
+// Problem  : 914A - Perfect Squares
+// Category : Brute force
+//============================================================================
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    set<int> st;
+    int n,x,ans;
+    for(int i = 0; i * i <= 1000000; i++)
+        st.insert(i*i);
+    while(cin >> n)
+    {
+        ans = INT_MIN;
+        for(int i = 0; i < n; i++)
+        {
+            cin >> x;
+            if(st.find(x) == st.end())
+                ans = max(ans, x);
+        }
+        cout << ans << '\n';
+    }
+    return 0;
+}
